783d586702ffc3cd28a6e6998ff9f6a465362d8f
rainbowda <930211680@qq.com>
2021-08-22 17:07:34 +0800

    py code

--- case/geopandas/README.md
+++ case/geopandas/README.md
@@ -1,1 +1,4 @@
 地图
+see https://zhuanlan.zhihu.com/p/80210579
+
+install geopandas see https://zhuanlan.zhihu.com/p/137628480
---
+++ case/geopandas/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import geopandas
+import matplotlib.pyplot as plt
+
+world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+world.plot()
+plt.show()
