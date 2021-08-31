5788e48d5b062438c804f2ef9e9dacebcb2520a9
rainbowda <930211680@qq.com>
2021-08-31 21:50:15 +0800

    word转pdf 版本1

--- word2pdf/main.py
+++ word2pdf/main.py
@@ -11,3 +11,31 @@ def createPdf(wordPath, pdfPath):
                             CreateBookmarks=constants.wdExportCreateHeadingBookmarks)
     word.Quit(constants.wdDoNotSaveChanges)
 createPdf(r"test.doc","test.pdf")
+
+
+
+
+'''
+from win32com.client import Dispatch
+from os import walk
+
+wdFormatPDF = 17
+
+
+def doc2pdf(input_file):
+    word = Dispatch('Word.Application')
+    doc = word.Documents.Open(input_file)
+    doc.SaveAs(input_file.replace(".doc", ".pdf"), FileFormat=wdFormatPDF)
+    doc.Close()
+    word.Quit()
+
+
+if __name__ == "__main__":
+    doc_files = []
+    directory = r"D:\jfb"
+    for root, dirs, filenames in walk(directory):
+        for file in filenames:
+            if file.endswith(".doc") or file.endswith(".docx"):
+                print(file)
+                doc2pdf(str(root + "\\" + file))
+'''
